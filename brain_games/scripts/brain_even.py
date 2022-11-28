cbbd88a92d9e380f1c437b2f6220b70165d64702
octode <octode@gmail.com>
2022-11-29 00:10:16 +0300

    Extracted generic functions from brain_even.py to common res file brain_res.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python3
 import random
-import prompt
-
-
-def greet():
-    """Prints welcome message on screen"""
-    print('Welcome to the Brain Games!')
-
-
-def welcome_user():
-    """Prompts username and prints welcome message, returns name"""
-    name = prompt.string('May I have your name? ')
-    name = name.capitalize()
-    print(f'Hello, {name}!')
-    return name
+from brain_res import *
 
 
 def even_rules_explain():
