1cf3f4de03763a1a7b1cb4ba0c8cad155b3c212c
Octode <octode@gmail.com>
2022-12-08 13:04:47 +0300

    Lint corretions to brain_engine.py

--- brain_games/brain_engine.py
+++ brain_games/brain_engine.py
@@ -19,18 +19,22 @@ def play(game):
     current_round = 1
 
     while current_round <= rounds_count:
+
         question, correct_answer = game.make_question_and_answer()
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
 
         if answer == correct_answer:
+
             print('Correct!')
             current_round += 1
+
         else:
-            print(f"""{answer} is wrong answer ;(. 
-            Correct answer was {correct_answer}.
-            Let's try again, {name}!
-            """)
+
+            print(f"'{answer}' is wrong answer ;(.", end=" ")
+            print(f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+
             return
 
     print(f'Congratulations, {name}!')
