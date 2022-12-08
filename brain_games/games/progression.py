e0d28bae895f948dd0ef9419c5a52a862c68747c
Octode <octode@gmail.com>
2022-12-08 17:10:11 +0300

    Lint refactoring

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,6 +7,23 @@ import random
 RULES = 'What number is missing in the progression?'
 
 
+def generate_math_progression():
+    """Generate math progression within defined params.
+
+        Returns: list of math progression string elements
+    """
+    progression_length = random.randint(5, 10)
+    element = random.randint(1, 50)
+    step = random.randint(2, 6)
+    progression = [str(element)]
+
+    for _ in range(1, progression_length):
+        element = element + step
+        progression.append(str(element))
+
+    return progression
+
+
 def make_question_and_answer():
     """Generate question and check for right answer.
 
@@ -14,21 +31,10 @@ def make_question_and_answer():
         question = random math progression with missing element.
         correct_answer = string with missing element.
     """
-    progression_length = random.randint(5, 10)
-    element = random.randint(1, 50)
-    step = random.randint(2, 6)
-    progression = [element]
-
-    for _ in range(1, progression_length):
-
-        element = element + step
-        progression.append(element)
-
-    hidden_element_position = random.randint(0, progression_length)
-    modified_progression = [str(el) for el in progression]
-    modified_progression[hidden_element_position] = '..'
-
-    question = ' '.join(modified_progression)
-    correct_answer = str(progression[hidden_element_position])
+    progression = generate_math_progression()
+    hidden_element_index = random.randint(0, len(progression) - 1)
+    correct_answer = progression[hidden_element_index]
+    progression[hidden_element_index] = '..'
+    question = ' '.join(progression)
 
     return question, correct_answer
